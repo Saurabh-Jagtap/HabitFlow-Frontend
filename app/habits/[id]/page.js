54bf630fe91a0e4a9134c2c8b04538310de0d27d
Shruti J <Jagtapshruti46@gmail.com>
2025-12-21 20:10:38 +0530

    Update app metadata, implement authentication middleware, and create initial pages for dashboard, habits, login, and register

---
+++ app/habits/[id]/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const habits = () => {
+  return (
+    <div>
+      Habit details
+    </div>
+  )
+}
+
+export default habits
---
+++ middleware.js
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(request) {
+  const { pathname } = request.nextUrl;
+
+  const isPublicRoute =
+    pathname === '/' ||
+    pathname.startsWith('/login') ||
+    pathname.startsWith('/register');
+
+  if (isPublicRoute) {
+    return NextResponse.next();
+  }
+
+  const token = request.cookies.get('accessToken');
+
+  if (!token) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/dashboard/:path*', '/habits/:path*'],
+};
